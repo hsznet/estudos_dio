688fd62719ef51387c1d61f91a6ee7edaf7e6d20
hsznet <hsznet72@gmail.com>
2022-02-24 00:12:24 -0300

    Novo commit com exercício de Filter e renomeação do de Map

---
+++ JavaScript/JS_Map_Filter_Reduce/Exercício_Filter.js
@@ -0,0 +1,5 @@
+const frutas = ["maçã verde", "maçã azul", "coco", "morango", "limão"];
+
+var maças = frutas.filter((maças) =>  maças.includes("maçã"));
+
+console.log(maças);
---
+++ JavaScript/JS_Map_Filter_Reduce/Exercício_Map.js
@@ -0,0 +1,11 @@
+const array = [1,2,3,4,5];
+
+
+var doubles = array.map(function(num) 
+{
+  return num * 2;
+
+});
+
+
+console.log(doubles);
